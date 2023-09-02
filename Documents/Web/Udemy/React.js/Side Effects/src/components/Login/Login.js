09b9def3447959f6d3c21298a9f986856a8fbc85
margauxlacheze <margaux.lchz@gmail.com>
2023-09-02 23:30:13 +0200

    UseEffect + UseReducer

---
+++ Documents/Web/Udemy/React.js/Side Effects/src/components/Login/Login.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect, useReducer } from 'react';
+
+import Card from '../UI/Card/Card';
+import classes from './Login.module.css';
+import Button from '../UI/Button/Button';
+
+const Login = (props) => {
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
+  const [enteredPassword, setEnteredPassword] = useState('');
+  const [passwordIsValid, setPasswordIsValid] = useState();
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  //Cette fonction peut être déclarée outside of the scope parce que pas d'interaction avec quoique ce soit de défini dans le composant
+  const emailReducer = (state, action) => {
+    if (action.type === 'USER_INPUT') {
+      return {value: action.val, isValid: action.val.includes('@')};
+    }
+    if (action.type === 'INPUT_BLUR') {
+      return {value: state.value, isValid: state.value.includes('@')}; // on ne laisse pas value vide car l'input peut blur une fois que l'user a mis quelque chose
+    }
+    return {value: '', isValid: false};
+  };
+
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value : '',
+    isValid: null
+  });
+
+  // useEffect(() => {
+  //   const identifier = setTimeout(()=>{
+  //     setFormIsValid(
+  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6
+  //     );
+  //   }, 500);
+  //   return () => {
+  //     clearTimeout(identifier);
+  //   }; // Clean up function
+  // }, [enteredEmail, enteredPassword]);
+
+  const emailChangeHandler = (event) => {
+    dispatchEmail({type: 'USER_INPUT', val: event.target.value});
+
+    setFormIsValid(
+      event.target.value.includes('@') && enteredPassword.trim().length > 6
+    );
+  };
+
+  const passwordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+
+    setFormIsValid(
+      emailState.isValid && event.target.value.trim().length > 6
+    );
+  };
+
+  const validateEmailHandler = () => {
+    dispatchEmail({type: 'INPUT_BLUR'}); // Pas obligé de mettre une val car on veut juste que l'input perde le focus
+  };
+
+  const validatePasswordHandler = () => {
+    setPasswordIsValid(enteredPassword.trim().length > 6);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    props.onLogin(emailState.value, enteredPassword);
+  };
+
+  return (
+    <Card className={classes.login}>
+      <form onSubmit={submitHandler}>
+        <div
+          className={`${classes.control} ${
+            emailState.isValid === false ? classes.invalid : ''
+          }`}
+        >
+          <label htmlFor="email">E-Mail</label>
+          <input
+            type="email"
+            id="email"
+            value={emailState.value}
+            onChange={emailChangeHandler}
+            onBlur={validateEmailHandler}
+          />
+        </div>
+        <div
+          className={`${classes.control} ${
+            passwordIsValid === false ? classes.invalid : ''
+          }`}
+        >
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={enteredPassword}
+            onChange={passwordChangeHandler}
+            onBlur={validatePasswordHandler}
+          />
+        </div>
+        <div className={classes.actions}>
+          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+            Login
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default Login;
